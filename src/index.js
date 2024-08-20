51a6d00d0c0eadad60e90054f8ba72fee52b8ba1
lyio067 <131585694+lyio067@users.noreply.github.com>
2024-08-20 19:17:18 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const dockerHub = "https://registry-1.docker.io";
 const routes = {
   // production
   "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "quay.lina.42web.io": "https://quay.io",
+  "gcr.lina.42web.io": "https://gcr.io",
+  "k8s-gcr.lina.42web.io": "https://k8s.gcr.io",
+  "k8s.lina.42web.io": "https://registry.k8s.io",
+  "ghcr.lina.42web.io: "https://ghcr.io",
+  "cloudsmith.lina.42web.io": "https://docker.cloudsmith.io",
+  "ecr.lina.42web.io": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
